66dab4d969fa6f6adf2925f8ea3a034bdc4b74c0
Bogdan Luca <luca.bogdan@gmail.com>
2018-09-27 16:11:19 +0300

    Another greeting update.

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hellooo from CodeSandbox SSE!!!</h1>
+    <h1>Hello from CodeSandbox SSE!</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <Link to="/page-2/">Go to page 2</Link>
